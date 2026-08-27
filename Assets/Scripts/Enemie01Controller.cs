f654de924aa8361957bef4d60a1f81b166d87647
dreamnexusstudios23 <rapidcrowgames@gmail.com>
2026-08-26 22:05:13 -0300

    EnemyShot Create

--- Assets/Scripts/Enemie01Controller.cs
+++ Assets/Scripts/Enemie01Controller.cs
@@ -9,8 +9,15 @@ public class Enemie01Controller : MonoBehaviour
     //Variável para pegar componentes do inimigo 01
     [SerializeField] private Rigidbody2D meuRB;
 
+    //Variável para pegar objetos do jogo
+    [SerializeField] private GameObject tiroInimigo;
+
     //Variáveis de movimento
-    [SerializeField] private float velocidade = 3f;
+    [SerializeField] private float velocidade = 1f;
+    [SerializeField] private float shotVel = 4f;
+
+    //Variáveis do timer do tiro
+    [SerializeField] private float shotTimer = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,6 +32,33 @@ public class Enemie01Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Usa os métodos
+        EnemyShot(); //Método do tiro do inimigo
+    }
+
+    //MÉTODOS
+
+    private void EnemyShot() //Método do tiro do inimigo
+    {
+        Debug.Log(shotTimer);
+
+        //Diminuo o timer se ele ainda não for zero
+        if (shotTimer > 0) shotTimer -= Time.deltaTime;
+
+        //SE o timer chegou em zero, ele cria o tiro
+        if (shotTimer <= 0)
+        {
+            //Crio a instancia do tiro em uma variável e faço ele nascer na minha posição no meu X e no meu Y
+            GameObject novoTiro = Instantiate(tiroInimigo, transform.position, Quaternion.identity);
+
+            //Pego o RigidBody do tiro
+            Rigidbody2D rbTiro = novoTiro.GetComponent<Rigidbody2D>();
+
+            //Dou velocidade a ele
+            rbTiro.linearVelocity = new Vector2(0f, -shotVel);
+
+            //Reinicio o timer
+            shotTimer = 2f;
+        }
     }
 }

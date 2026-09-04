5eec3a1d473fc70eea3394a1a6c300d675f400a4
dreamnexusstudios23 <rapidcrowgames@gmail.com>
2026-09-03 23:02:49 -0300

    UI

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
@@ -18,6 +19,10 @@ public class GameController : MonoBehaviour
     [SerializeField] private float yMin = 6f; //Variável que define o valor máximo do nascimento do inimigo no eixo Y
     [SerializeField] private float yMax = 10f; //Variável que define o valor máximo do nascimento do inimigo no eixo Y
     private int arrayInd = 0; //Variável que cuida do indice do array dos inimigos : MÉTODO WaveCreate()
+
+    //Variáveis de TEXTO
+    [SerializeField] private TextMeshProUGUI textoPontos; //Pega o texto que exibe a pontuação
+    [SerializeField] private TextMeshProUGUI textoLevel; //Pega o texto que exibe o level
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -94,10 +99,27 @@ public class GameController : MonoBehaviour
     //Método de ganhar pontos
     public void GanhaPontos(int pontos)
     {
+        //OBJETIVO DO MÉTODO
+        /*
+         * Fazer ganhar pontos de acordo com o parâmetro
+         * Ganhar level a cada 100 PONTOS
+         * Exibir o texto do LEVEL e dos PONTOS
+         * Aumenta o tempo de criação das WAVES de acordo com o level
+        */
+
         //Ganho pontos
         this.pontos += pontos;
 
         //SE meu pontos forem aumentando de 100 em 100, eu subo de level
         level = (this.pontos / 100) + 1;
+
+        //Exibe o texto dos pontos de acordo com os pontos atuais
+        textoPontos.text = Mathf.Round(this.pontos).ToString();
+
+        //Exibe o texto do level
+        textoLevel.text = Mathf.Round(level).ToString();
+
+        //Aumenta o tempo de espera a cada level que passa
+        waitTime = level * 8f; 
     }
 }

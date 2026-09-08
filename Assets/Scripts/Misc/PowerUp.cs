cfa8cae20539dc49c0d3a54015ba28742dec1c76
dreamnexusstudios23 <rapidcrowgames@gmail.com>
2026-09-07 21:36:39 -0300

    Making PowerUp animation

--- Assets/Scripts/Misc/PowerUp.cs
+++ Assets/Scripts/Misc/PowerUp.cs
@@ -11,6 +11,7 @@ public class PowerUp : MonoBehaviour
 
     //Variáveis de controle
     private int vel = 1; //Velocidade de movimento do powerup
+    private float timerDestroy = 3f; //Timer para me auto-destruir após 3 segundos
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,7 +26,8 @@ public class PowerUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //USA OS MÉTODOS
+        SelfDestroy();
     }
 
     //SEMPRE QUE EU COLIDIR COM O PLAYER eu aumento o nível do tiro dele
@@ -48,6 +50,37 @@ public class PowerUp : MonoBehaviour
             //Me destruo depois
             Destroy(gameObject, 0.01f);
 
+        }
+    }
+
+    //MÉTODOS//
+
+    //Método de me destruir após 3 segundos
+    private void SelfDestroy()
+    {
+        //OBJETIVO DO MÉTODO
+        /*
+         * Depois que eu fui criado, eu conto 3 segundos até me destruir e sumir
+         */
+
+        //Diminui o timer se ele já não estiver em 0
+        if (timerDestroy > 0) timerDestroy -= Time.deltaTime;
+
+        //SE chegar em 0 o timer
+        if (timerDestroy <= 0)
+        {
+            //Eu me destruo
+            //Depois de fazer isso eu reproduzo a animação (Anim do impacto do tiro)
+            GameObject particula = Instantiate(destroyAnim, transform.position, Quaternion.identity);
+
+            //Destruo a particula
+            Destroy(particula, 1f);
+
+            //Me destruo depois
+            Destroy(gameObject, 0.01f);
+
+            //Reseto o timer só por precaução
+            timerDestroy = 3f;
         }
     }
 }
